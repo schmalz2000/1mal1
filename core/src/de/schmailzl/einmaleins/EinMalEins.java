d93ab56ad5231d9424820dab80aec307e5fc55ec
Sebastian Schmailzl <sebi.2000@t-online.de>
2018-06-17 19:04:16 +0200

    Test Commit

--- core/src/de/schmailzl/einmaleins/EinMalEins.java
+++ core/src/de/schmailzl/einmaleins/EinMalEins.java
@@ -1,19 +1,16 @@
 package de.schmailzl.einmaleins;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class EinMalEins extends ApplicationAdapter {
+public class EinMalEins extends Game {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,13 +18,6 @@ public class EinMalEins extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
